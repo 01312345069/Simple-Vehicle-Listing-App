32baf4ca62b24cb50463244255493258e0b99291
Md Naymul Islam মোঃ নয়িমুল ইসলাম <mybspi@gmail.com>
2024-12-28 20:05:53 +0600

    header added

---
+++ public/views/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vehicle Listing</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <h1>Vehicle Listing App</h1>
